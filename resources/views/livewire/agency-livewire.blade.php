8341492e9d255b84b1da029bfa0b995cf14ba039
rtrenuela <rtrenuela@objectbright.com>
2022-09-21 15:04:00 +0800

    update

--- resources/views/livewire/agency-livewire.blade.php
+++ resources/views/livewire/agency-livewire.blade.php
@@ -1,10 +1,17 @@
 <div>
+    @push('breadcrumbs')
+        <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Agencies</li>
+        </ol>
+        <h6 class="font-weight-bolder mb-0">Agencies</h6>
+    @endpush
     <livewire:toaster/>
     <div class="card mt-5">
         <div class="card-body">
             <div class="card-title mb-4 d-flex flex-row">
                 <h3>Agencies</h3>
-                <x-a-button class="btn btn-success ml-3">
+                <x-a-button class="btn btn-success ms-3">
                     <x-slot name="others">
                         data-bs-toggle="modal" data-bs-target="#agencyModal" wire:click="$set('details', [])"
                     </x-slot>
