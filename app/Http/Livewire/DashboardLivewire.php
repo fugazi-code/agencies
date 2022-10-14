87b1d4e1f7d199fc3a50c3ae8baaf80f4ec9f16e
rtrenuela <rtrenuela@objectbright.com>
2022-10-14 09:17:54 +0800

    update

--- app/Http/Livewire/DashboardLivewire.php
+++ app/Http/Livewire/DashboardLivewire.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Candidate;
 use App\Models\Voucher;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class DashboardLivewire extends Component
 {
     public int $totalVoucher = 0;
+    public int $totalCandidate = 0;
 
-    public function render()
+    public function mount()
     {
         $this->totalVoucher = Voucher::query()->where('agency_id', auth()->user()->agency_id)->count();
+        $this->totalCandidate = Candidate::query()->where('agency_id', auth()->user()->agency_id)->count();
+    }
 
+    public function render(): Factory|View|Application
+    {
         return view('livewire.dashboard');
     }
 }
--- resources/views/buttons/link.blade.php
+++ resources/views/buttons/link.blade.php
@@ -1,5 +1,12 @@
 <div class="d-grid">
-    <a href="{{ $link }}" class="btn btn-sm btn-link m-0" {!! $attr ?? '' !!}>
-        {!! html_entity_decode($label) !!}
-    </a>
+    @isset($link)
+        <a href="{{ $link }}" class="btn btn-sm btn-link m-0" {!! $attr ?? '' !!}>
+            {!! html_entity_decode($label) !!}
+        </a>
+    @else
+        <button type="button" class="btn btn-sm btn-link m-0" {!! $attr ?? '' !!}>
+            {!! html_entity_decode($label) !!}
+        </button>
+    @endisset
+
 </div>
