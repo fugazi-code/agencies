23fb47ad6acf0b0776ae63a23e3871c423ba0265
rtrenuela <rtrenuela@objectbright.com>
2022-10-15 11:29:22 +0800

    update

--- resources/views/livewire/cases.blade.php
+++ resources/views/livewire/cases.blade.php
@@ -11,7 +11,8 @@
         <div class="card-body">
             <div class="card-title mb-4 d-flex flex-row">
                 <h3>Cases</h3>
-                <a href="{{ route('complaint-form-livewire', ['agencyId' => 1]) }}" class="btn btn-outline-info ms-3" target="_blank">
+                <a href="{{ route('complaint-form-livewire', ['agencyId' => 1]) }}" class="btn btn-outline-info ms-3"
+                   target="_blank">
                     Complaint Form
                 </a>
             </div>
@@ -23,47 +24,48 @@
         </div>
     </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="caseDetailModal" tabindex="-1" aria-labelledby="caseDetailModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="caseDetailModalLabel">Case Detail</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        @if($detail)
-                            <div class="row">
-                                <div class="col-md-12 d-flex flex-row justify-content-center">
-                                    @if($detail['image1'])
-                                        <a href="{{ $detail['image1'] }}" class="btn btn-sm btn-info mx-2" target="_blank">
-                                            Image 1
-                                        </a>
-                                    @endif
-                                    @if($detail['image2'])
-                                        <a href="{{ $detail['image2'] }}" class="btn btn-sm btn-info mx-2" target="_blank">
-                                            Image 2
-                                        </a>
-                                    @endif
-                                    @if($detail['image3'])
-                                        <a href="{{ $detail['image3'] }}" class="btn btn-sm btn-info mx-2" target="_blank">
-                                            Image 3
-                                        </a>
-                                    @endif
-                                </div>
-                                <div class="col-md-12 border-top">
-                                    Complaint
-                                    <p>
-                                        {!! $detail['complaint'] !!}
-                                    </p>
-                                </div>
+    <!-- Modal -->
+    <div wire:ignore.self class="modal fade" id="caseDetailModal" tabindex="-1" aria-labelledby="caseDetailModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="caseDetailModalLabel">Case Detail</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @if($detail)
+                        <div class="row">
+                            <div class="col-md-12 d-flex flex-row justify-content-center">
+                                @if($detail['image1'])
+                                    <a href="{{ $detail['image1'] }}" class="btn btn-sm btn-info mx-2" target="_blank">
+                                        Image 1
+                                    </a>
+                                @endif
+                                @if($detail['image2'])
+                                    <a href="{{ $detail['image2'] }}" class="btn btn-sm btn-info mx-2" target="_blank">
+                                        Image 2
+                                    </a>
+                                @endif
+                                @if($detail['image3'])
+                                    <a href="{{ $detail['image3'] }}" class="btn btn-sm btn-info mx-2" target="_blank">
+                                        Image 3
+                                    </a>
+                                @endif
                             </div>
-                        @endif
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
+                            <div class="col-md-12 border-top">
+                                Complaint
+                                <p>
+                                    {!! $detail['complaint'] !!}
+                                </p>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
+    </div>
 </div>
